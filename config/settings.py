a5d895fb8e066b655437106e869a6565d71df050
Kirienko Eugene <wtff6a@gamil.com>
2020-02-21 10:32:15 +0600

    tested with email, its works

--- config/settings.py
+++ config/settings.py
@@ -115,5 +115,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'neobis.b00k1ng@gmail.com'
+EMAIL_HOST_PASSWORD = 'kdsyxccrlcjfkjbi'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
