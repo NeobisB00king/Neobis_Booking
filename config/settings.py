e31e55356e897bb5a725a3550869e2489def589b
Kirienko Eugene <wtff6a@gamil.com>
2020-02-27 23:49:36 +0600

    delete signup, non-worked app rooms with new static, templates. Need to handle templates

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'feedback'
+    'feedback',
+    # 'rooms',
 ]
 
 MIDDLEWARE = [
