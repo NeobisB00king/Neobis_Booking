0abc56b7cf0804189394ee658156f6d98bbbf58b
AkimC <akim.duyshenaliev@gmail.com>
2020-03-12 22:37:50 +0600

    Rebuilt some files

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-import django
-import sphinx_rtd_theme
+import sys, os, django, sphinx_rtd_theme
+# import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 # os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 # django.setup()
