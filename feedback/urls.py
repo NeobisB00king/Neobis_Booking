d7419ce6f5cf906a4b2dcdcb3b347668e6605187
AkimC <akim.duyshenaliev@gmail.com>
2020-03-13 15:32:40 +0600

    Changed URL of feedback a bit

--- feedback/urls.py
+++ feedback/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
-    path('feedback', HomeView.as_view(), name='Feedback'),
+    path('', HomeView.as_view(), name='Feedback'),
     path('img', ImageUploadView.as_view(), name='ImageUpload'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
