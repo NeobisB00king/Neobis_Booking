294c736b491775a04429c00d93788e5133c3eb95
AkimC <akim.duyshenaliev@gmail.com>
2020-04-09 15:17:47 +0600

    Ready for frontend

--- booking_rest/settings.py
+++ booking_rest/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jet_django',
+    # 'jet_django',
     'feedback',
     'booking',
     'rest_framework',
