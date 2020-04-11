4571b5bf5c6312cdf127c3cbc64f880d0254facb
AkimC <akim.duyshenaliev@gmail.com>
2020-04-11 14:21:52 +0600

    switched to postgresql

--- booking_rest/settings.py
+++ booking_rest/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 import locale
+import dj_database_url
 from decouple import config
 
 config.encoding = locale.getpreferredencoding(False)
@@ -112,11 +113,17 @@ WSGI_APPLICATION = 'booking_rest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Password validation
